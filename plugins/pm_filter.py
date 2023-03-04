d04d24909bb211ea57bacbd3d2d8d8c36baac0fd
sᴛᴀʀ ʙᴏɪ <119308395+its-star-boi@users.noreply.github.com>
2023-03-04 16:13:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -496,7 +496,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ident, file_id = query.data.split("#")
         files_ = await get_file_details(file_id)
         if not files_:
-            return await query.answer('I Like Your Smartness, But Don't Be Oversmart Okay 😒')
+            return await query.answer("I Like Your Smartness, But don't Be Oversmart Okay 😒")
         files = files_[0]
         title = files.file_name
         size = get_size(files.file_size)
